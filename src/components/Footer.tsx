015ce0408db0d8bea3dc08e83787fa4ce384ba3c
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-12-23 18:23:17 +0300

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,23 @@
 const Footer = () => {
   return (
     <div>
-      
+      <div>MyTasks</div>
+      <div>
+      <ul>
+        <li>About us</li>
+        <li>Contact</li>
+        <li>Services</li>
+        <li><button>Login</button></li>
+        <li><button>sign up</button></li>
+        </ul>
+      </div>
+      <div>
+        <li>facebook</li>
+        <li>instagram</li>
+        <li>twitter</li>
+        <li>linkedin</li>
+      </div>
+      <div>WWW.MyTasks.com copyright @ 2024.All rights reserved</div>
     </div>
   )
 }
