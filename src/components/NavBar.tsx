cd509e721c7973c658b8b3a5012f7a2aed4e40d0
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-12-24 16:03:10 +0300

    changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,16 @@
 const NavBar = () => {
   return (
     <div>
-      
+      <div>MyTasks</div>
+      <div>
+        <ul>
+        <li>About us</li>
+        <li>Contact</li>
+        <li>Services</li>
+        <li><button>Login</button></li>
+        <li><button>sign up</button></li>
+        </ul>
+      </div>
     </div>
   )
 }
