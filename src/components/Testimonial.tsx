6d7124c94e3c11e5c1d8ae8a09b77bc5566bac8f
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-12-24 16:03:25 +0300

    changes

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -3,7 +3,21 @@
 const Testimonial = () => {
   return (
     <div>
-      
+      <div>
+         <img src="" alt="" />
+         <p>I love the website as it helps me schedule my tasks</p>
+         <p>Mary khalid</p>
+      </div>
+      <div>
+        <img src="" alt="" />
+         <p>I have used MyTasks for quite some time and i have enjoyed the services</p>
+         <p>Paul Kamau</p>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <p>i cannot regret using MyTasks which helps me out in organizing my work</p>
+        <p>Evah Otieno</p>
+      </div>
     </div>
   )
 }
