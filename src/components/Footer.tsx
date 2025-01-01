c4ff782bfa0a5d20f5d2a748cafad5d3c95671cd
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2025-01-01 17:16:41 +0300

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 
 const Footer = () => {
   return (
-    <div>
-      <div>MyTasks</div>
-      <div>
+    <>
+    <div className="footer text-center px-5 md:text-left md:grid md:grid-cols-2 lg:py-10">
+      <div className="block mx-auto pt-10 md:mx-0" >MyTasks</div>
+      <div className="md:grid md:grid-cols-2 lg:grid-cols-3">
       <ul>
         <li>About us</li>
         <li>Contact</li>
@@ -11,15 +12,15 @@ const Footer = () => {
         <li><button>Login</button></li>
         <li><button>sign up</button></li>
         </ul>
-      </div>
-      <div>
+        <ul>
         <li>facebook</li>
         <li>instagram</li>
         <li>twitter</li>
         <li>linkedin</li>
-      </div>
-      <div>WWW.MyTasks.com copyright @ 2024.All rights reserved</div>
+        </ul>
+      WWW.MyTasks.com copyright @ 2024.All rights reserved</div>
     </div>
+    </>
   )
 }
 
