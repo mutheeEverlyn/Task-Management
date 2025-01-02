b63346e0988d5e22ea91e6c5753a5426b592c790
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2025-01-02 17:58:46 +0300

    added concepts

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,24 +1,54 @@
-
+const testimonialData=[
+  {
+    name:"Mary khalid",
+    image:"",
+    description:"I love the website as it helps me schedule my tasks"
+  },
+  {
+  name:"Paul Kamau",
+  image:"",
+  description:"I have used MyTasks for quite some time and i have enjoyed the services"
+  },
+  {
+  name:"Evah Otieno",
+  image:"",
+  description:"I cannot regret using MyTasks which helps me out in organizing my work"
+  }
+]
 
 const Testimonial = () => {
   return (
-    <div>
-      <div>
-         <img src="" alt="" />
-         <p>I love the website as it helps me schedule my tasks</p>
-         <p>Mary khalid</p>
+    <>
+       <div className=" py-14 sm:pb-24">
+        <div className="w-full max-w-full">
+          <div className="space-y-4 pb-12">
+            <p className="text-3xl font-semibold text-center sm:text-4xl font-serif" >
+              What Our Clients Say About Us
+            </p>
+            <p  className="text-center sm:px-44">
+             Here are some of the feedbacks we received from our clients
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ">
+            {testimonialData.map((testimony) => (
+              <div
+                key={testimony.name}
+                className="card text-center group space-y-3 sm:space-y-6 p-4 sm:py-12  bg-gray-100 duration-300  rounded-lg "
+              >
+                <div className="grid place-items-center ">
+                  <img src={testimony.image}  className="rounded-full w-20 h-20"/>
+ 
+                </div>
+                <div className="text-2xl">⭐⭐⭐⭐⭐</div>
+                <p>{testimony.description}</p>
+                <p className="text-center font-semibold">{testimony.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <div>
-        <img src="" alt="" />
-         <p>I have used MyTasks for quite some time and i have enjoyed the services</p>
-         <p>Paul Kamau</p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p>i cannot regret using MyTasks which helps me out in organizing my work</p>
-        <p>Evah Otieno</p>
-      </div>
-    </div>
+    </>
   )
 }
 
