04c0436f113abb9ec887de896af46fe1404e09d0
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2025-01-02 17:58:37 +0300

    added concepts

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,35 @@
-
+import hamburger from '../assets/icon-hamburger.svg'
+import { useState } from 'react'
+import close from '../assets/icon-close.svg'
+import  {Link}  from 'react-router-dom'
+import MobileMenu from './MobileMenu'
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
-    <div>
-      <div>MyTasks</div>
-      <div>
-        <ul>
-        <li>About us</li>
-        <li>Contact</li>
-        <li>Services</li>
-        <li><button>Login</button></li>
-        <li><button>sign up</button></li>
-        </ul>
+    <div className='flex gap-10 bg-gray-600 text-white   w-full  relative  shadow-md duration-300 md:items-center justify-between h-20  md:gap-0 '>
+      <div className='w-32 lg:w-40 md:w-32 '>MyTasks</div>
+      <nav className='hidden  md:flex md:justify-between md:gap-1 '>
+      <ul className='flex   gap-5 items-center md:gap-3 '>
+        <li><button><Link to='/' className='hover:border-b-2 hover:text-blue-400'>HOME</Link></button></li>
+        <li><button><Link to='/about'  className='hover:border-b-2 hover:text-blue-400'>ABOUT US</Link></button></li>
+        <li><button><Link to='/services'  className='hover:border-b-2 hover:text-blue-400'>SERVICES</Link></button></li>
+        <li><button><Link to='/contact-us'  className='hover:border-b-2 hover:text-blue-400'>CONTACT US</Link></button></li>
+      </ul>
+      <div className='flex gap-5 items-center md:gap-2 lg:ml-40'>
+      <button className=' bg-blue-400  hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 my-10'><Link to='/login'> LOGIN</Link></button>
+      <button className='bg-blue-400  hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 my-10 '><Link to='/register'>REGISTER</Link></button>
       </div>
+      </nav>
+      {showMenu?(
+       <button onClick={toggleMenu} className='cursor-pointer transition-all '><img src={close} alt='close icon' className='bg-transparent  m-auto md:hidden h-10 '/></button>
+      ):(
+        <button onClick={toggleMenu} className=' cursor-pointer transition-all' >< img src={hamburger} alt='icon humburger' className='bg-transparent  m-auto md:hidden h-10 '/></button>
+      )}
+      <MobileMenu showItems={showMenu}/>
     </div>
   )
 }
