ac8177d6de06b019d5c454d305b4117379e25420
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2025-01-05 19:32:02 +0330

    changes

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import TasksSummary from './TasksSummary';
 const Admin: React.FC = () => {
-
   return (
     <div className='min-h-screen'>
-      
+      <TasksSummary/>
     </div>
   );
 };
