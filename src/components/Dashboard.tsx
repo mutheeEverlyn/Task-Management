d8b58d49cc8f8dff9b0b2f3814342a95dbdbdfec
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2025-01-05 19:32:28 +0330

    changes

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,15 @@
+import React from 'react';
+import { useOutletContext } from 'react-router-dom';
+import MyTasksChart from './MyTasksChart';
 
-const Dashboard = () => {
+const Dashboard: React.FC = () => {
+  const { user_id } = useOutletContext<{ user_id: number }>();
+
   return (
-    <div>
-      
+    <div className='min-h-screen'>
+      <MyTasksChart user_id={user_id} />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
