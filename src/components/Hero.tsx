608e68c325ce3dcd08864a627eb515ccb0dc84f2
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2025-01-10 15:11:54 +0330

    changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import HeroPic from "../assets/HeroPic.jpg"
 const Hero = () => {
   return (
     <>
@@ -5,11 +6,11 @@ const Hero = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
         <div className="space-y-5 order-1 sm:order-1 sm:pr-32 ">
           <p className="text-blue-400 text-2xl font-serif">
-              Welcome to MyTasks.This platform helps you to schedule your tasks according to the order of importance to help meet your work timeline
+              Welcome to MyTasks.This platform helps you to manage your Tasks effectively
             </p>
         </div>
         <div className="order-2 sm:order-2">
-           <img src="" alt="" />
+           <img src={HeroPic} alt="hero pic" />
         </div>
       </div>
     </div> 
